9f0d70db421a0356e25ab27143b2ed256aef485c
GustavoGiga123 <110988741+GustavoGiga123@users.noreply.github.com>
2022-09-19 12:53:10 +0000

    ,snebfg

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,36 @@
 <template>
 <div>
   
-<div class="columns">
+<div class="columns mt-5">
   <div class="column is-8">
           <b-tabs>
-        <b-tab-item label="Storys"> 
+        <b-tab-item label="Feed"> 
+
+          
+          <div class="columns mt-4">
+      <div class="column is-3">
+        <article class="notification is-white has-text-centered">
+          <b-image class="image" :src="require('@/assets/1.jpg')"></b-image>
+          <p class="subtitle mt-3">Zé do Pão</p>
+        </article>
+      </div>
+            
+      <div class="column is-3 has-text-centered">
+        <article class="notification is-white">
+          <b-image class="image" :src="require('@/assets/2.jpg')"></b-image>
+          <p class="subtitle mt-3">Yelong Ma</p>
+        </article>
+      </div>
+      <div class="column is-3">
+        <article class="notification is-white has-text-centered">
+          <b-image class="image" :src="require('@/assets/3.jpg')"></b-image>
+          <p class="subtitle mt-3">Craudete</p>
+        </article>
+      </div>      
+
+
+            
+    </div>
         </b-tab-item>
 
         <b-tab-item label="Reels">
@@ -17,11 +43,52 @@
       </b-tabs>
   </div>
   <div class="column is-4">
-    Second column
+    
+          <h2 class= "subtitle">Aniversários</h2> 
+          <section class= "mt-3">
+            <p>Tipo:</p>
+         <article class="media">
+        <figure class="media-left">
+          <p class="image is-64x64">
+            <img src="https://media-exp2.licdn.com/dms/image/C4E03AQGBRQCqe5QlBw/profile-displayphoto-shrink_200_200/0/1605848126920?e=2147483647&v=beta&t=K0BuRgHYZUfqJdA3VDWzHVRhJngtAMVRXky6xcYh15M" />
+          </p>
+        </figure>
+        <div class="media-content">
+          <div class="content">
+            <p>
+              <strong>Calesso</strong> <small>@Calesso69</small>
+              <small>31m</small>
+              <br />
+             Nascido em 19/09/2003
+            </p>
+          </div>
+        </div>
+      </article>
+          </section>
+
+          <section class= "mt-3">
+            <p>Contatos</p>
+            <article class="media">
+        <figure class="media-left">
+          <p class="image is-64x64">
+            <img src="https://media-exp1.licdn.com/dms/image/C4E03AQGFreFQL4-OIg/profile-displayphoto-shrink_800_800/0/1628851036470?e=2147483647&v=beta&t=1P8RhOqRa_rrMb1elYdB1GjJOHnq62ZfNmoHN5jp0DQ" />
+          </p>
+        </figure>
+        <div class="media-content">
+          <div class="content">
+            <p>
+              <strong>Adad</strong> <small>@duduadad</small>
+              <small>31m</small>
+            
+            </p>
+          </div>
+        </div>
+      </article>
+          </section>
+    
   </div>
 </div>
   
 </div>
 </template>
-´´
 
