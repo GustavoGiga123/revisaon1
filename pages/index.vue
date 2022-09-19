d861c4d080c25c56edb089c2c956230e11cf2a94
GustavoGiga123 <110988741+GustavoGiga123@users.noreply.github.com>
2022-09-19 11:46:07 +0000

    qualquer merda

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,27 @@
 <template>
-  <section class="section">
-    <h1 class="title">Página inicial</h1>
-  </section>
+<div>
+  
+<div class="columns">
+  <div class="column is-8">
+          <b-tabs>
+        <b-tab-item label="Storys"> 
+        </b-tab-item>
+
+        <b-tab-item label="Reels">
+          Lorem <br />
+          ipsum <br />
+          dolor <br />
+          sit <br />
+          amet.
+        </b-tab-item>
+      </b-tabs>
+  </div>
+  <div class="column is-4">
+    Second column
+  </div>
+</div>
+  
+</div>
 </template>
+´´
 
